eacfae407e319e6f5fcbf13c1690c6aef4ecedc3
Timon Lilje <timon.lilje@gmail.com>
2023-09-05 17:39:53 +0200

    fix serial connection bug

--- Keypad.ino
+++ Keypad.ino
@@ -1,8 +1,8 @@
 #include "Keyboard.h"
 
 //pins
-int row[4] = { 0, 1, 2, 3 };
-int col[3] = { 4, 5, 6 };
+const int row[4] = { 0, 1, 2, 3 };
+const int col[3] = { 4, 5, 6 };
 
 bool keysPressed[4][3];
 bool lastKeysPressed[4][3];
@@ -10,29 +10,28 @@ bool lastKeysPressed[4][3];
 //keycodes for single keypress
 //https://theasciicode.com.ar/
 const char keyMap[4][3] = {
-  { 65, 66, 67 },
-  { 68, 69, 70 },
-  { 71, 72, 73 },
-  { 0, 48, 0 }
+  { KEY_F13, KEY_F14, KEY_F15 },
+  { KEY_F16, KEY_F17, KEY_F18 },
+  { KEY_F19, KEY_F20, KEY_F21 },
+  { KEY_F22, KEY_F23, KEY_F24 }
 };
 
 //only for key assignments
 const int keys[4][3] = {
-  { 0, 1, 2 },
-  { 3, 4, 5 },
-  { 6, 7, 8 },
-  { 9, 10, 11 }
+  { 1, 2, 3 },
+  { 4, 5, 6 },
+  { 7, 8, 9 },
+  { 10, 11, 12 }
 };
 
-const int numpadKey[] = {KEY_KP_0, KEY_KP_1, KEY_KP_2, KEY_KP_3, KEY_KP_4, KEY_KP_5, KEY_KP_6, KEY_KP_7, KEY_KP_8, KEY_KP_9};
+const int numpadKey[] = { KEY_KP_0, KEY_KP_1, KEY_KP_2, KEY_KP_3, KEY_KP_4, KEY_KP_5, KEY_KP_6, KEY_KP_7, KEY_KP_8, KEY_KP_9 };
 
 void setup() {
   Serial.begin(115200);
-  while (!Serial);
+  while (!Serial && millis() < 1000);
+  Serial.println("serial initialized");
 
   Keyboard.begin(KeyboardLayout_de_DE);
-
-  Serial.println("serial initialized");
 
   for (int i = 0; i < 4; i++) {
     pinMode(row[i], OUTPUT);
@@ -50,22 +49,12 @@ void loop() {
     for (int j = 0; j < 3; j++) {
       //key assignments:
       switch (keys[i][j]) {
-
         //special macros
-        case 0:
+        case 1:
           numpadAscii(i, j, 231);
           break;
-        case 1:
-          numpadAscii(i, j, 232);
-          break;
-        case 2:
-          numpadAscii(i, j, 233);
-          break;
-        case 9:
+        case 10:
           textMacro(i, j, "max.mustermann@generic-mailservice.com");
-          break;
-        case 11:
-          numpadAscii(i, j, 236);
           break;
 
         //all other keys: press key according to keyMap
